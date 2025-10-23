1792652c9ca944d12ba854e6aa195670281748d2
HaasFynn <fynn.haas@ergon.ch>
2025-10-23 14:56:22 +0200

    Implement update and delete

--- src/main/java/ch/zli/m223/controller/EntryController.java
+++ src/main/java/ch/zli/m223/controller/EntryController.java
@@ -2,40 +2,57 @@ package ch.zli.m223.controller;
 
 import java.util.List;
 
-import javax.inject.Inject;
-import javax.ws.rs.Consumes;
-import javax.ws.rs.GET;
-import javax.ws.rs.POST;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.MediaType;
-
-import org.eclipse.microprofile.openapi.annotations.Operation;
-import org.eclipse.microprofile.openapi.annotations.tags.Tag;
-
 import ch.zli.m223.model.Entry;
 import ch.zli.m223.service.EntryService;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
 @Path("/entries")
 @Tag(name = "Entries", description = "Handling of entries")
 public class EntryController {
 
-    @Inject
-    EntryService entryService;
+  @Inject
+  EntryService entryService;
 
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Operation(summary = "Index all Entries.", description = "Returns a list of all entries.")
-    public List<Entry> index() {
-        return entryService.findAll();
-    }
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  @Operation(summary = "Index all Entries.", description = "Returns a list of all entries.")
+  public List<Entry> index () {
+    return entryService.findAll();
+  }
 
-    @POST
-    @Produces(MediaType.APPLICATION_JSON)
-    @Consumes(MediaType.APPLICATION_JSON)
-    @Operation(summary = "Creates a new entry.", description = "Creates a new entry and returns the newly added entry.")
-    public Entry create(Entry entry) {
-       return entryService.createEntry(entry);
-    }
+  @POST
+  @Path("/create")
+  @Produces(MediaType.APPLICATION_JSON)
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Operation(summary = "Creates a new entry.", description = "Creates a new entry and returns the newly added entry.")
+  public Entry create (Entry entry) {
+    return entryService.createEntry(entry);
+  }
 
+  @PUT
+  @Path("/update")
+  @Produces(MediaType.APPLICATION_JSON)
+  @Consumes(MediaType.APPLICATION_JSON)
+  @Operation(summary = "Updates an existing entry.", description = "Updates an existing entry and returns the status of the update.")
+  public boolean update (Entry entry) {
+    return entryService.updateEntry(entry);
+  }
+
+  @DELETE
+  @Path("/delete/{id}")
+  @Produces(MediaType.APPLICATION_JSON)
+  public boolean delete(@PathParam("id") Long id) {
+    return entryService.deleteEntry(id);
+  }
 }
