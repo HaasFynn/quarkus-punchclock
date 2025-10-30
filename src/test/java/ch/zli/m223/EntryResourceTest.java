09391e91f02c25eb9d2e4d2fd2f138b2e62bd75d
HaasFynn <fynn.haas@ergon.ch>
2025-10-30 09:06:18 +0100

    Implement Unit Tests

--- src/test/java/ch/zli/m223/EntryResourceTest.java
+++ src/test/java/ch/zli/m223/EntryResourceTest.java
@@ -1,16 +1,19 @@
 package ch.zli.m223;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.notNullValue;
 
 import io.quarkus.test.junit.QuarkusTest;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
 
 @QuarkusTest
-public class EntryResourceTest {
+class EntryResourceTest {
 
   @Test
-  public void testIndexEndpoint () {
+  void testIndexEndpoint () {
     given()
       .when().get("/entries")
       .then()
@@ -33,7 +36,13 @@ public class EntryResourceTest {
       .body(json)
       .when().post("/entries/create")
       .then()
-      .statusCode(200);
+      .statusCode(200)
+      .body("id", notNullValue())
+      .body("checkIn", equalTo("2025-10-24T08:30:00"))
+      .body("checkOut", equalTo("2025-10-24T18:30:00"))
+      .body("category.id", equalTo(1))
+      .body("category.name", equalTo("category1"))
+      .body("tags", hasSize(0));
   }
 
 }
